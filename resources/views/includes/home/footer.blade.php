6f9fa650a79a2579ab6c896a219f7ab825effe24
Brownie <matt.brown@boxsix.co.uk>
2019-07-31 14:45:19 +0100

    Small edits done

--- resources/views/includes/home/footer.blade.php
+++ resources/views/includes/home/footer.blade.php
@@ -27,33 +27,23 @@
         </div>
       </div>
         <footer class="footer" id="contact">
+          <br>
           <div class="row">
-            <div class="col-12 col-lg-4">
-              <p class="font-weight-bold"><u>Contact</u></p>
-              <p><i class="fas fa-phone"></i> 0773 05086858</p><p><i class="far fa-envelope"></i> retroworlds@gmail.com</p>
-              <a class="nav-link" href="https://www.facebook.com/OnlineRetroGames/" target="_blank"> <i class="fab fa-facebook-square fa-2x" style="color: #3C5A99;"></i> Find Us on Facebook</a>
-            </div>
-            <div class="col-12 col-lg-4">
-              <p class="font-weight-bold"><u>Legal</u></p>
-              <p><a href="{{ route('privacy.index')}}">Privacy Policy</a></p>
-              <p><a href="{{ route('cookies.index')}}">Cookie Policy</a></p>
-              <p><a href="{{ route('terms.index')}}">Terms & Conditions</a></p>
-            </div>
-            <div class="col-12 col-lg-4">
-              <p class="font-weight-bold"><u>Address</u></p>
-              <p>Retro World - Mrs Jane Tiochta (3/4/1398)</p>
-              <p>C/O NMTF</p>
-              <p>Hampton House</p>
-              <p>Hoyland, Barnsley</p>
-              <p>S740HA</p>
-            </div>
-
-
+            <div class="col-2 offset-2"><a href="{{ route('privacy.index')}}">Privacy Policy</a></div>
+            <div class="col-2"><a href="{{ route('cookies.index')}}">Cookie Policy</a></div>
+            <div class="col-2"><a href="{{ route('terms.index')}}">Terms & Conditions</a></div>
+            <div class="col-2"><a href="{{ route('terms.index')}}">FAQ</a></div>
           </div>
-          <img src="/images/paypal.png" alt="" class="img-fluid" style="width: 20%">
+          <br>
+          <div class="row">
+            <div class="col-10 offset-1">
+              <p>Retro World - Mrs Jane Tiochta (3/4/1398), c/o NMTF, Hampton House, Hoyland, Barnsley, S740HA, <br><i class="fas fa-phone"></i> 077305086858, &nbsp<i class="far fa-envelope"></i> retroworlds@gmail.com <a href="https://www.facebook.com/OnlineRetroGames/" target="_blank"> <i class="fab fa-facebook-square fa-2x" style="color: #3C5A99;"></i> Find Us on Facebook</a></p>
+              <img src="/images/paypal.png" alt="" class="img-fluid" style="width: 20%">
+            </div>
+          </div>
+
         </footer>
       </div>
-s
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
